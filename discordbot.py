96f43aa1d1f18e6c5a70ee8d42e675c52303d07f
eda3 <gerorin1010@gmail.com>
2019-09-19 05:50:38 +0900

    add: function r1d100(), roll()

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import random
+
+from typing import List
 
 bot = commands.Bot(command_prefix=';')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -19,6 +22,28 @@ async def ping(ctx):
 @bot.command()
 async def houka(ctx):
     await ctx.send(':fire:')
+
+
+@bot.command()
+async def r1d100(ctx):
+    dice_count: int = 1
+    dice_men: int = 100
+    dice_results: List[int] = []
+    result_sum: int = 0
+
+    result_sum = await roll(dice_count, dice_men, dice_results, result_sum)
+    await ctx.send(result_sum)
+
+
+async def roll(dice_count, dice_men, dice_results, result_sum):
+
+    for i in range(dice_count):
+        print('i:', i)
+        result: int = random.randint(1, dice_men)
+        dice_results.append(result)
+        result_sum += dice_results[i]
+
+    return result_sum
 
 
 bot.run(token)
